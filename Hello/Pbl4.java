5cee27092e2ccee55246cf2e6406c636fead69f3
Dell <Dell@DESKTOP-BID1P7O>
2020-07-05 21:54:02 +0530

    First Commit

---
+++ Hello/Pbl4.java
@@ -0,0 +1,23 @@
+public class Pbl4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=Integer.parseInt(args[0]);
+		if(a>0)
+		{
+			System.out.println("it is a Postive number");
+		
+	}
+		else if(a<0)
+		{
+			System.out.println("it is a negative number");
+			
+		}
+		else if(a==0)
+		{
+			System.out.println("it's a zero");
+		}
+
+}
+}
+
